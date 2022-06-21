3a4733e85c8cc47e35c61d4c4f6d6f054b8fdb0c
Ben <benphillips96@outlook.com>
2022-06-21 16:00:46 +0100

    added a navbar file, very basic navbar

---
+++ sbd_cinema_project/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavBar = () => {
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" href="#">Listings</a>
+        <a class="nav-link active" href="#">Upcoming</a>
+        <a class="nav-link active" href="#">Location</a>
+      </div>
+    </div>
+  </div>
+</nav>
+}
