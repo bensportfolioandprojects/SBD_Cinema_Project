8c63baa32631b996b717a0802b5457cf1d4e9a50
Redy <redy@live.co.uk>
2022-06-22 12:27:34 +0100

    Edited opening times

--- sbd_cinema_project/src/components/openingtimes/OTTable.jsx
+++ sbd_cinema_project/src/components/openingtimes/OTTable.jsx
@@ -1,15 +1,17 @@
 const OTTable = () => {
     return ( 
         <div className="timesTable">
-            <h4>
-                Mon: 12:00 - 20:00
-                Tue: 12:00 - 20:00
-                Wed: 12:00 - 20:00
-                Thu: 12:00 - 20:00
-                Fri: 12:00 - 23:00
-                Sat: 12:00 - 23:00
-                Sun: 12:00 - 18:00
-            </h4>
+            <ul>
+                <h4>
+                    <li>Mon: 12:00 - 20:00</li>
+                    <li>Tue: 12:00 - 20:00</li>
+                    <li>Wed: 12:00 - 20:00</li>
+                    <li>Thu: 12:00 - 20:00</li>
+                    <li>Fri: 12:00 - 23:00</li>
+                    <li>Sat: 12:00 - 23:00</li>
+                    <li>Sun: 12:00 - 18:00</li>
+                </h4>
+            </ul>
         </div>
      );
 }
