3d345428344f5a5615a571afadc7a46838c07128
Redy <redy@live.co.uk>
2022-06-22 13:34:14 +0100

    Adjustments to Contact Pages

---
+++ sbd_cinema_project/src/contactus.jsx
@@ -0,0 +1,15 @@
+import ContactTitle from "./components/contactus/ContactTitle";
+import Email from "./components/contactus/Email";
+import PostalAddress from "./components/contactus/PostalAddress";
+
+const contactus = () => {
+    return ( 
+        <>
+            <ContactTitle/>
+            <Email/>
+            <PostalAddress/>
+        </>
+     );
+}
+ 
+export default contactus;
