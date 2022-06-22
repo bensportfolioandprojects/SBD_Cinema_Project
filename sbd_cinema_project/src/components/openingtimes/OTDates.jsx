d423ab671be9de8fab1b2717d0fdcfc7a22845e8
Redy <redy@live.co.uk>
2022-06-22 16:52:32 +0100

    Times edited

--- sbd_cinema_project/src/components/openingtimes/OTDates.jsx
+++ sbd_cinema_project/src/components/openingtimes/OTDates.jsx
@@ -2,9 +2,9 @@ const OTDates = () => {
     return ( 
         <div className="timesDates">
             <h4>We are closed on the following days:
-                16th June
-                10th October
-                30th November
+                16th June -
+                10th October -
+                30th November -
                 5th December
             </h4>
         </div>
