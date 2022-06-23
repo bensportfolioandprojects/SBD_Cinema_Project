5f0c577f79e275609246bf5f3783302aacdd8bf6
Ben <benphillips96@outlook.com>
2022-06-23 11:39:52 +0100

    directors added, MVP now met

--- sbd_cinema_project/src/components/newreleasegallery/NewReleases.jsx
+++ sbd_cinema_project/src/components/newreleasegallery/NewReleases.jsx
@@ -11,6 +11,7 @@ const NewReleases = () => {
                 <div> <a href="./TImage.html"><TImage/></a> </div>
                 <h2>Thor embarks on a journey unlike anything he's ever faced -- a quest for inner peace. However, his retirement gets interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods.</h2>
                 <h5>Starring Chris Hemsworth,Natalie Portman,Tessa Thompson</h5>
+                <h5>Director - Taika Waititi</h5>
                 <h5>Running time 2hrs 13mins</h5>
                 <div id= "Release Date">
                     <h3>Release Date - 7 Jul 2022</h3>
@@ -18,7 +19,8 @@ const NewReleases = () => {
             <h1>The Railway Children Return</h1>
                 <div> <a href="./RCRImage.html"><RCRImage/></a> </div>
                 <h2>'The Railway Children Return' will take audiences on an exciting and heart-warming journey, in which a new group of children are evacuated to a Yorkshire village during World War 2.</h2>
-                <h5>Jenny Agutter, Sheridan Smith, Tom Courtenay</h5>
+                <h5>Starring Jenny Agutter, Sheridan Smith, Tom Courtenay</h5>
+                <h5>Director - Morgan Matthews</h5>
                 <div id= "Release Date">
                     <h3>Release Date -  15 Jul 2022</h3>
                 </div>
@@ -26,6 +28,7 @@ const NewReleases = () => {
                 <div> <a href="./BTImage.html"><BTImage/></a> </div>
                 <h2>Five assassins find themselves on a fast moving bullet train from Tokyo to Morioka with only a few stops in between. They discover their missions are not unrelated to each other.</h2>
                 <h5>Starring Brad Pitt,Andrew Koji,Joey King</h5>
+                <h5>Director - David Leitch</h5>
                 <div id= "Release Date">
                     <h3>Release Date - 3 Aug 2022</h3>
                 </div>
@@ -33,6 +36,7 @@ const NewReleases = () => {
                 <div> <a href="./SMImage.html"><SMImage/></a> </div>
                 <h2>Miles Morales returns for an epic adventure that will transport Brooklyn's full-time, friendly neighborhood Spider-Man across the Multiverse to join forces with Gwen Stacy and a new team of Spider-People.</h2>
                 <h5>Starring Shameik Moore,Hailee Steinfeld,Oscar Isaac</h5>
+                <h5>Directors - Joaquim Dos Santos, Kemp Powers, Justin Thompson</h5>
                 <div id= "Release Date">
                     <h3>Release Date - 7 Oct 2022</h3>
                 </div>
