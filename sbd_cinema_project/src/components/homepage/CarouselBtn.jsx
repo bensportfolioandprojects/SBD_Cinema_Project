3d17d5aff43205348607b608f17785a72864e790
pezantish <e.e.a.sadek@gmail.com>
2022-06-23 20:33:52 +0100

    ES: Bootstrap Carousel added, resizing carousel seems beyond me

--- sbd_cinema_project/src/components/homepage/CarouselBtn.jsx
+++
@@ -1,7 +0,0 @@
-const CarouselBtn = ({data}) => {
-    return (
-        <input value={data.text} type="button" placeholder="state" onClick={data.listener}/>
-    );
-}
- 
-export default CarouselBtn;
--- sbd_cinema_project/src/components/homepage/Cinema.jsx
+++ sbd_cinema_project/src/components/homepage/Cinema.jsx
@@ -1,45 +1,46 @@
-import Listing from "./Listing";
-import CarouselBtn from "./CarouselBtn";
+// import Listing from "./Listing";
+// import CarouselBtn from "./CarouselBtn";
 import DefaultImg from '../../resources/PureSeduction.jpg';
 import DefaultImg2 from '../../resources/tw1sted.png';
+import Carousel from 'react-bootstrap/Carousel';
 import {useState} from "react";
 
 const Cinema = () => {
 
-    const[stateValue, stateSetter]=useState(1);
+    // const[stateValue, stateSetter]=useState(1);
 
-    const listings = [
-        {
-           link: "#",
-           title: "James and the Mediocre Beach",
-           img: DefaultImg,
-        },
-        {
-            link: "#",
-           title: "Larry Dotter and the Mug of Water",
-           img: DefaultImg2,
-        },
-        {
-            link: "#",
-           title: "Attacking Public Smith",
-           img: DefaultImg,
-        },
-        {
-            link: "#",
-           title: "Sergeant England",
-           img: DefaultImg2,
-        }
-    ]
+    // const listings = [
+    //     {
+    //        link: "#",
+    //        title: "James and the Mediocre Beach",
+    //        img: DefaultImg,
+    //     },
+    //     {
+    //         link: "#",
+    //        title: "Larry Dotter and the Mug of Water",
+    //        img: DefaultImg2,
+    //     },
+    //     {
+    //         link: "#",
+    //        title: "Attacking Public Smith",
+    //        img: DefaultImg,
+    //     },
+    //     {
+    //         link: "#",
+    //        title: "Sergeant England",
+    //        img: DefaultImg2,
+    //     }
+    // ]
     
-    let func = (val, inc, setter) => {
-        if (val + inc < 1){
-            setter(4);
-        }else if (val + inc > 4 ){
-            setter(1);
-        }else{
-            setter(val + inc)
-        }
-    };
+    // let func = (val, inc, setter) => {
+    //     if (val + inc < 1){
+    //         setter(4);
+    //     }else if (val + inc > 4 ){
+    //         setter(1);
+    //     }else{
+    //         setter(val + inc)
+    //     }
+    // };
     
     // function MyComponent(props){
     //     useEffect(()=>{
@@ -50,25 +51,45 @@ const Cinema = () => {
 
     // setTimeout(function () { func(stateValue, -1, stateSetter);}, 10000);
 
-    const buttons = [
-        {
-            text: "prev",
-            listener: function () { func(stateValue, -1, stateSetter);},
-        },
-        {
-            text: "next",
-            listener: function () { func(stateValue, 1, stateSetter);}
-        },
-    ]
-    
+    // const buttons = [
+    //     {
+    //         text: "prev",
+    //         listener: function () { func(stateValue, -1, stateSetter);},
+    //     },
+    //     {
+    //         text: "next",
+    //         listener: function () { func(stateValue, 1, stateSetter);}
+    //     },
+    // ]
+    // return (
+        // <div id="hp-cinema">
+        //     <div id="hp-viewport">
+        //         <CarouselBtn data={buttons[0]}/>
+        //         <CarouselBtn data={buttons[1]}/>
+        //         <Listing data={listings[stateValue-1]}/>
+        //     </div> 
+        // </div> 
     return (
-        <div id="hp-cinema">
-            <div id="hp-viewport">
-                <CarouselBtn data={buttons[0]}/>
-                <CarouselBtn data={buttons[1]}/>
-                <Listing data={listings[stateValue-1]}/>
-            </div> 
-        </div> 
+    <Carousel id="hello" fade>
+        <Carousel.Item>
+            <img src={DefaultImg}/>
+            <Carousel.Caption>
+                <p>Hello</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src={DefaultImg2}/>
+            <Carousel.Caption>
+                <p>Goodbye</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src={DefaultImg}/>
+            <Carousel.Caption>
+                <p>Hello</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
     );
 }
  
