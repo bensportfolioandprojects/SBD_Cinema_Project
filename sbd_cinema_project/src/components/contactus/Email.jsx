27e0cc0ad12d7c1b5ae6d319fb6b0793165a6176
Redy <redy@live.co.uk>
2022-06-23 12:13:45 +0100

    Email.jsx modified

--- sbd_cinema_project/src/components/contactus/Email.jsx
+++ sbd_cinema_project/src/components/contactus/Email.jsx
@@ -29,7 +29,7 @@ function Email() {
 
     return (
         <div className="Email">
-            <form id="contact-form" method="POST">
+            <form id="contact-form" method="POST" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
                     <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} />
