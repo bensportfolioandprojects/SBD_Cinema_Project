a33571e02bf607cd06480aba67dde7f2f0e81293
Ben <benphillips96@outlook.com>
2022-06-23 11:24:41 +0100

    added directors to match spec, MVP reached

--- sbd_cinema_project/src/components/listingsgallery/Listings.jsx
+++ sbd_cinema_project/src/components/listingsgallery/Listings.jsx
@@ -11,6 +11,7 @@ const Listings = () => {
                 <div> <a href="./JWDImage.html"><JWDImage/></a> </div>
                 <h2>This summer, experience the epic conclusion to the Jurassic era as two generations unite for the first time.</h2>
                 <h5>Starring Chris Pratt, Bryce Dallas Howard, Sam Neill, Jeff Goldblum, Laura Dern</h5>
+                <h5>Director - Colin Trevorrow</h5>
                 <h5>Running time 2hrs 27mins</h5>
                 <div id= "times">
                     <h3>Showing times:</h3>
@@ -29,6 +30,7 @@ const Listings = () => {
                 <div> <a href="./LImage.html"><LImage/></a> </div>
                 <h2>The definitive origin story of Buzz Lightyear, the hero who inspired the toy, Lightyear follows the legendary Space Ranger after he is marooned on a hostile planet 4.2 million light-years from Earth alongside his commander and their crew.</h2>
                 <h5>Starring Chris Evans, Keke Palmer, Taika Waititi, Uzo Aduba, Dale Soules</h5>
+                <h5>Director - Angus MacLane</h5>
                 <h5>1hr 40mins</h5>
                 <div id= "times">
                     <h3>Showing times:</h3>
@@ -46,6 +48,7 @@ const Listings = () => {
                 <div> <a href="./TGMImage.html"><TGMImage/></a> </div>
                 <h2>Set 34 years after the events of Top Gun, Tom Cruise returns in his iconic role as Maverick. Joined by Jennifer Connelly, Val Kilmer, and Miles Teller, this action-packed sequel produced by Jerry Bruckheimer will be sure to satisfy your need for speed.</h2>
                 <h5>Starring Tom Cruise, Jennifer Connelly, Jon Hamm, Miles Teller, Val Kilmer</h5>
+                <h5>Director - Joseph Kosinski</h5>
                 <h5>Running time 2hrs 11mins</h5>
                 <div id= "times">
                     <h3>Showing times:</h3>
@@ -62,6 +65,7 @@ const Listings = () => {
                 <div> <a href="./DSImage.html"><DSImage/></a> </div>
                 <h2>Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary..</h2>
                 <h5>Starring Benedict Cumberbatch, Elizabeth Olsen, Benedict Wong, Rachel McAdams, Chiwetel Ejiofor, Xochitl Gomez</h5>
+                <h5>Director - Sam Raimi</h5>
                 <h5>Running time 2hrs 6mins</h5>
                 <div id= "times">
                     <h3>Showing times:</h3>
