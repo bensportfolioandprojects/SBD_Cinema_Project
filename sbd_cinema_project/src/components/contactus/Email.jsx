ca4e8e9bdf3b8e1be527c8017d61d40f2ee49237
Redy <redy@live.co.uk>
2022-06-24 11:33:42 +0100

    Email form functionally complete

--- sbd_cinema_project/src/components/contactus/Email.jsx
+++ sbd_cinema_project/src/components/contactus/Email.jsx
@@ -1,45 +1,52 @@
 import { useState } from 'react';
 import { emailRegex } from './emailRegex';
 import axios from 'axios';
+import { Popover } from 'react-bootstrap';
+import { OverlayTrigger} from 'react-bootstrap';
 
 function Email() {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-    
+    const [value, setValue] = useState('');
+
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Body>
+                {value}
+            </Popover.Body>
+        </Popover>
+    );
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         if (!email.length) {
-            return {
-                status: true,
-                value: "Email is required"
-            };
-        } else if (!emailRegex.test(email)) {
+            console.log("Hello");
+            setValue("Please enter an Email address");
+        } else if (!email.match(emailRegex)) {
             console.log(email);
-            return {
-                status: true,
-                value: "Email is invalid"
-            };
-        }
+            setValue("Please enter a VALID Email address");
+        } else {
+            setValue("Valid Email address");
+
         return await axios({
             url: 'http://localhost:3002/send',
             method: "POST",
             headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            data: JSON.stringify({name, email, message})
+            data: JSON.stringify({ name, email, message })
         }).then((response) => {
-            if(response.data.status === 'success') {
+            if (response.data.status === 'success') {
                 alert("Message Sent.");
-              } else if (response.data.status === 'fail') {
+            } else if (response.data.status === 'fail') {
                 alert("Message failed to send.")
-              }
-        return response.data;
-    }).catch((err) => console.log(err))
-    };
+            }
+            return response.data;
+        }).catch((err) => console.log(err))
+    }};
 
     return (
         <div className="Email">
@@ -50,17 +57,19 @@ function Email() {
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={email} onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="message">Message</label>
-                    <textarea className="form-control" rows="5" id="message" value={message} onChange={e => setMessage(e.target.value)}/>
+                    <textarea className="form-control" rows="5" id="message" value={message} onChange={e => setMessage(e.target.value)} />
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </OverlayTrigger>
             </form>
         </div>
-        );
-    }
+    );
+}
 
 
 export default Email;
