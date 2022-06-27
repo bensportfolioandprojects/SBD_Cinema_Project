0aa005191d42890026c7e5a78cd6f4e197935534
Ben <benphillips96@outlook.com>
2022-06-27 16:57:16 +0100

    added CSS styling to most components on the page and added scrum logo, need to organise the logo so that it aligns with the quote

--- sbd_cinema_project/src/components/AboutSBDHeader.jsx
+++ sbd_cinema_project/src/components/AboutSBDHeader.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function AboutSBDHeader() {
     return (
         <div className="aboutsbdheader--container">
-        <h1><u>SilentButDeadly</u></h1>
-        <img src="https://www.kali.org/tools/sbd/images/sbd-logo.svg" alt="SBDLogo"/>
+        <h1>SilentButDeadly</h1>
         </div>
     )
 }
