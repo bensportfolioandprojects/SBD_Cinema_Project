b7f95e22aa0e277c16674a6a9d9f9325a1c565b5
Ben <benphillips96@outlook.com>
2022-06-28 16:41:16 +0100

    added CSS styling and resized the images so they fit better on the page

--- sbd_cinema_project/src/components/screens/ScreensBody.jsx
+++ sbd_cinema_project/src/components/screens/ScreensBody.jsx
@@ -6,14 +6,16 @@ import ScreensImage from "./ScreensImage";
 
 const ScreensBody = () => {
     return (
+        <div>
         <div className="screensbody--container">
             <ScreensImage />
-        <p>We want our seating to be comfortable enough for you to feel at home. When you're watching the end of the world or a mind blowing romance, you shouldn't have to worry about uncomfortable chairs or not being able to see over the person in front of you.</p>
-        <p>This is why we worked with a team of specialised interior designers to create a layout using the best of furniture that makes your experience the best. </p>
+        <h4>We want our seating to be comfortable enough for you to feel at home. When you're watching the end of the world or a mind blowing romance, you shouldn't have to worry about uncomfortable chairs or not being able to see over the person in front of you.<br/>
+        This is why we worked with a team of specialised interior designers to create a layout using the best of furniture that makes your experience the best. </h4>
             <ScreensDecorImage />
-        <div>
+        </div>
+        <div className="deluxe">
             <h2>For the luxury lovers...</h2>
-            For the taste of luxury, watch your new favourite films from our fully inclusive directors delux lounge. With your own personal chef and butler attending to your every need and leather heated reclining massage chairs ensuring that you get the best experience possible. Whilst the layout of the delux screen is the same as our standard screen, as you can see below it is extravagance at its finest.
+            <h4>For the taste of luxury, watch your new favourite films from our fully inclusive directors delux lounge. With your own personal chef and butler attending to your every need and leather heated reclining massage chairs ensuring that you get the best experience possible. Whilst the layout of the delux screen is the same as our standard screen, as you can see below it is extravagance at its finest.</h4>
             <DeluxeScreensImage />
         </div>
         <h6>Our screens all have disabled access and seating with multiple fire exits.</h6>
