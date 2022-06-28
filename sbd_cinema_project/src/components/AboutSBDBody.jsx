ca12b420ca26fa6bcfa3d159ce660f9c13dca391
Ben <benphillips96@outlook.com>
2022-06-28 10:42:43 +0100

    attempted addition of flexbox, but added some minor adjustments to the layout of the logo and the quoe

--- sbd_cinema_project/src/components/AboutSBDBody.jsx
+++ sbd_cinema_project/src/components/AboutSBDBody.jsx
@@ -9,7 +9,7 @@ function AboutSBDBody() {
             We are currently training under QA Ltd and will soon be working at the MET Office in Devon, England.
             </p>
         </div>
-        <p  className="members">
+        <div  className="members">
             <h2> Members of the team:
                 <ul>
            Ben Phillips (Scrum Master) - @BenPhillipsQAC - Ben@QACinemas.com<br/>
@@ -18,7 +18,7 @@ function AboutSBDBody() {
            Richard Edwin de Young - @RichardEdwindeYoungQAC - Richard@QACinemas.com
                 </ul>
            </h2>
-        </p>
+        </div>
         <h3 className="sbdlink">
             Learn more about who we are and what we do: <a href ="www.SBDdesign.com">here</a>
         </h3>
@@ -31,17 +31,20 @@ function AboutSBDBody() {
             </p>
         </div>
         
-        <p className="scrumquote">
-            As stated on the <a href="https://www.agilealliance.org/wp-content/uploads/2019/08/Scrumorg-Logo_tagline-TM.png">Scrum Website:</a><br/>
-            "In a nutshell, Scrum requires a Scrum Master to foster an environment where:
-        <ol>
-        <li>A Product Owner orders the work for a complex problem into a Product Backlog.</li>
-        <li>The Scrum Team turns a selection of the work into an Increment of value during a Sprint.</li>
-        <li>The Scrum Team and its stakeholders inspect the results and adjust for the next Sprint.</li>
-        <li> Repeat"</li>
-        </ol>
-        </p>
-        <img src="https://cdn.worldvectorlogo.com/logos/scrumorg-1.svg" alt="scrumlogo" className="scrumlogo"/>
+        <div className="scrum-flex">
+            <div className="scrumquote">
+                As stated on the <a href="https://www.agilealliance.org/wp-content/uploads/2019/08/Scrumorg-Logo_tagline-TM.png">Scrum Website:</a><br/>
+                "In a nutshell, Scrum requires a Scrum Master to foster an environment where:
+                <ol>
+                    <li>A Product Owner orders the work for a complex problem into a Product Backlog.</li>
+                    <li>The Scrum Team turns a selection of the work into an Increment of value during a Sprint.</li>
+                    <li>The Scrum Team and its stakeholders inspect the results and adjust for the next Sprint.</li>
+                    <li> Repeat"</li>
+                </ol>
+            </div>
+            <img src="https://cdn.worldvectorlogo.com/logos/scrumorg-1.svg" alt="scrumlogo" className="scrumlogo"/>
+        </div>
+        
             <p className="sprintinfo">
             We had two 5 day long sprints to produce the site. Throughout, the Scrum Master ran through daily sprint review meetings to go through what each member had finished and what each member needed to work on.<br/>
             We reached a point where some members had finished their slated work and so could help out with some of the heavier tasks. At the start of sprint 1, using planningpoker.com we assigned all user stories a value as to how difficult it would be to complete.
